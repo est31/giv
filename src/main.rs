b4a1b0cf6ad267e96d66dd8257f2566516da15cd
est31 <MTest31@outlook.com>
2025-12-29 03:19:14 +0100

    Print history

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ fn print_commits() -> Result<(), anyhow::Error> {
     let repo = gix::open(".")?;
     let head_commit = repo.head_commit()?;
     let msg = head_commit.message()?;
-    println!("head commit msg title: {}", msg.title);
+    let id = head_commit.id().shorten_or_id();
+    println!("Commit {} {}", id, msg.title);
+
+    let mut parent_ids = head_commit.parent_ids();
+    while let Some(parent_id) = parent_ids.next() {
+        let cmt = repo.find_commit(parent_id)?;
+        let msg = cmt.message()?;
+        let id = cmt.id().shorten_or_id();
+        println!("Commit {} {}", id, msg.title);
+    }
     Ok(())
 }
 
