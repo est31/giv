1518bfbab3922ef141c09fccb09d814a40706fee
est31 <MTest31@outlook.com>
2025-12-29 04:33:04 +0100

    print commit list via ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,100 @@
-fn print_commits() -> Result<(), anyhow::Error> {
-    let repo = gix::open(".")?;
-    let head_commit = repo.head_commit()?;
-    let msg = head_commit.message()?;
-    let id = head_commit.id().shorten_or_id();
-    let title = msg.title.to_string();
-    println!("Commit {} {}", id, title.trim());
+use std::time::Duration;
 
-    let budget = 10;
-    let mut commit = head_commit;
+use anyhow::{Context, anyhow};
+use crossterm::event::KeyCode;
+use gix::Repository;
+use ratatui::{
+    DefaultTerminal, crossterm::event, Frame,
+    text::Line,
+};
 
-    for _ in 0..budget {
-        // TODO support multiple parent IDs
-        let Some(parent_id) = commit.parent_ids().next() else {
-            // No parent left
-            break;
+struct State {
+    repo: Repository,
+}
+
+struct App {
+    state: State,
+    terminal: DefaultTerminal,
+}
+
+impl State {
+    fn new() -> Result<State, anyhow::Error> {
+        let state = State {
+            repo: gix::open(".")?,
         };
-        commit = repo.find_commit(parent_id)?;
-        let msg = commit.message()?;
-        let id = commit.id().shorten_or_id();
-        let title = msg.title.to_string();
-        println!("Commit {} {}", id, title.trim());
+        Ok(state)
     }
-    Ok(())
+    fn draw(&self, frame: &mut Frame) -> Result<(), std::io::Error> {
+        let lines = self.commits_lines()
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        let paragraph = ratatui::widgets::Paragraph::new(lines);
+        frame.render_widget(paragraph, frame.area());
+        Ok(())
+    }
+    fn commits_lines(&self) -> Result<Vec<Line<'_>>, anyhow::Error> {
+        let head_commit = self.repo.head_commit()?;
+        let msg = head_commit.message()?;
+        let id = head_commit.id().shorten_or_id();
+        let title = msg.title.to_string();
+        let mut res = Vec::new();
+        res.push(Line::from(format!("Commit {} {}", id, title.trim())));
+
+        let budget = 10;
+        let mut commit = head_commit;
+
+        for _ in 0..budget {
+            // TODO support multiple parent IDs
+            let Some(parent_id) = commit.parent_ids().next() else {
+                // No parent left
+                break;
+            };
+            commit = self.repo.find_commit(parent_id)?;
+            let msg = commit.message()?;
+            let id = commit.id().shorten_or_id();
+            let title = msg.title.to_string();
+            res.push(Line::from(format!("Commit {} {}", id, title.trim())));
+        }
+        Ok(res)
+    }
+}
+
+impl App {
+    fn new(terminal: DefaultTerminal) -> Result<App, anyhow::Error> {
+        let app = App {
+            state: State::new()?,
+            terminal,
+        };
+        Ok(app)
+    }
+    fn run(&mut self) -> Result<(), anyhow::Error> {
+        loop {
+            self.terminal.try_draw(|frame| self.state.draw(frame))?;
+            if event::poll(Duration::from_millis(100)).context("failed to poll for events")? {
+                match event::read().context("failed to read event")? {
+                    event::Event::Key(key) => {
+                        if key.code == KeyCode::Char('q') {
+                            // Quit the application using q
+                            break;
+                        }
+                    }
+                    event::Event::FocusGained => (),
+                    event::Event::FocusLost => (),
+                    event::Event::Mouse(_) => (),
+                    event::Event::Paste(_) => (),
+                    event::Event::Resize(_, _) => (),
+                }
+            }
+        }
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), anyhow::Error> {
-    print_commits()?;
+    color_eyre::install()
+        .map_err(|err| anyhow!("{}", color_eyre::Report::msg(err)))?;
+    let terminal = ratatui::init();
+    let mut app = App::new(terminal)?;
+    app.run()?;
+    ratatui::restore();
     Ok(())
 }
