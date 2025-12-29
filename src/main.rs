7a63f7a64a5ca7ae892b816990cbd96331f91add
est31 <MTest31@outlook.com>
2025-12-29 03:07:16 +0100

    print head commit msg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn print_commits() -> Result<(), anyhow::Error> {
+    let repo = gix::open(".")?;
+    let head_commit = repo.head_commit()?;
+    let msg = head_commit.message()?;
+    println!("head commit msg title: {}", msg.title);
+    Ok(())
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    print_commits()?;
+    Ok(())
 }
